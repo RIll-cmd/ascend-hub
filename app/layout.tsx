84abbbf0ad1b14873b03e46bf6bea0a390919e80
CyrillGerardCajucom <cyrillgerardc@gmail.com>
2026-09-25 20:56:41 +0800

    fix(client): shield dev overlay from third-party browser extension unhandled rejections

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                function isExtensionError(err) {
+                  if (!err) return false;
+                  var str = String((err && (err.stack || err.message)) || err);
+                  return (
+                    str.indexOf('chrome-extension://') !== -1 ||
+                    str.indexOf('moz-extension://') !== -1 ||
+                    str.indexOf('safari-extension://') !== -1 ||
+                    str.indexOf('M_ID') !== -1
+                  );
+                }
+                window.addEventListener('unhandledrejection', function(event) {
+                  if (isExtensionError(event.reason)) {
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                  }
+                }, true);
+                window.addEventListener('error', function(event) {
+                  if (
+                    isExtensionError(event.error) ||
+                    (event.filename && (
+                      event.filename.indexOf('chrome-extension://') !== -1 ||
+                      event.filename.indexOf('moz-extension://') !== -1
+                    ))
+                  ) {
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                  }
+                }, true);
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
